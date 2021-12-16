ef75e41cc7a758d360564ab8958f522890831772
Ryan Huang <59319094+Ryan-Huang1@users.noreply.github.com>
2021-12-16 10:38:36 -0500

    gradle update

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
-  public static final String APP_BUILD_TIME = "2021-12-16T09:35:43.599-0500";
+  public static final String APP_BUILD_TIME = "2021-12-16T10:08:39.648-0500";
 }
