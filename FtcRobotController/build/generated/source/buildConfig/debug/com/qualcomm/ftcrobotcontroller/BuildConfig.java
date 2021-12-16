54d35b9e033379febb637261cdd48448f1318873
Ryan Huang <59319094+Ryan-Huang1@users.noreply.github.com>
2021-12-16 11:08:38 -0500

    update color of ui

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
-  public static final String APP_BUILD_TIME = "2021-12-16T11:00:58.282-0500";
+  public static final String APP_BUILD_TIME = "2021-12-16T11:07:45.737-0500";
 }
